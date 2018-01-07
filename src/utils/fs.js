e05efa4b2bde95311c6e5577b249a309788e4f81
Rocky Wu <rwu823@gmail.com>
2018-01-07 14:04:30 +0800

    Fix readFile

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,6 +1,8 @@
 const { promisify } = require('util')
 const fs = require('fs')
 
-exports.readFile = promisify(fs.readFile).then(buf => buf.toString())
+exports.readFile = (...args) =>
+  promisify(fs.readFile)(...args).then(buf => buf.toString())
+
 exports.exists = promisify(fs.exists)
 exports.mkdir = promisify(fs.mkdir)
